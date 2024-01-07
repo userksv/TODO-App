9d7ec2d8c46cae4451cb3711b5945497cd2996c7
Mik <kimsergkorea@gmail.com>
2024-01-07 12:51:01 +0900

    trying deploy

--- pull_from_ecr.sh
+++ pull_from_ecr.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 docker pull 930849404806.dkr.ecr.ap-northeast-2.amazonaws.com/todo:backend
-docker pull 930849404806.dkr.ecr.ap-northeast-2.amazonaws.com/todo:redis
+docker pull 930849404806.dkr.ecr.ap-northeast-2.amazonaws.com/todo:frontend
