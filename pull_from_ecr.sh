ae522e8cdcc8fc67071aa60904d120ba7e85a8c3
Mik <kimsergkorea@gmail.com>
2024-01-07 19:33:53 +0900

    deploy-added celery

--- pull_from_ecr.sh
+++ pull_from_ecr.sh
@@ -2,3 +2,5 @@
 
 docker pull 930849404806.dkr.ecr.ap-northeast-2.amazonaws.com/todo:backend
 docker pull 930849404806.dkr.ecr.ap-northeast-2.amazonaws.com/todo:frontend
+docker pull 930849404806.dkr.ecr.ap-northeast-2.amazonaws.com/todo:worker
+docker pull 930849404806.dkr.ecr.ap-northeast-2.amazonaws.com/todo:beat
